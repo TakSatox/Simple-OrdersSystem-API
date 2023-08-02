e12d6ea62020e9a61b461b3f74f268778876b7bf
CARLOS SATO <carlos.sato@santodigital.com.br>
2023-08-01 23:32:22 -0300

    including txt file for personal purposes

--- tests/test_CRUD.py
+++ tests/test_CRUD.py
@@ -1,11 +1,10 @@
+from main import app
+from fastapi.testclient import TestClient
 import sys
 import os
 
 # Adicione o diretório raiz do projeto ao sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from main import app
-from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
