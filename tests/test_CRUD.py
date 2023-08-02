1a6d4a6be8d664a87c262227ffec5a553e308292
CARLOS SATO <carlos.sato@santodigital.com.br>
2023-08-01 23:11:47 -0300

    correcting response status in test_CRUD

--- tests/test_CRUD.py
+++ tests/test_CRUD.py
@@ -11,7 +11,7 @@ client = TestClient(app)
 
 def test_post_orders():
     response = client.post("/api/v1/orders", json={"product": "testing", "quantity": 1, "total_amount": 50.10})
-    assert response.status_code == 203
+    assert response.status_code == 201
     assert response.json() == {
         "product": "testing",
         "quantity": 1,
