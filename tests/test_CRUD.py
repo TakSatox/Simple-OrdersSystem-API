3aec96b25f2a48e01cb2b558eb184487d5958966
CARLOS SATO <carlos.sato@santodigital.com.br>
2023-08-01 22:52:36 -0300

    another test

--- tests/test_CRUD.py
+++ tests/test_CRUD.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Adicione o diretório raiz do projeto ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 from fastapi.testclient import TestClient
 
